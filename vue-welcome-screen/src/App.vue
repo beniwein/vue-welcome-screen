ba7f9846dd41c3eee3de246dd7107d773affa056
beniwein <beni_weinmann@hotmail.com>
2022-05-18 14:06:58 +0200

    remove-slice-A2url

--- vue-welcome-screen/src/App.vue
+++ vue-welcome-screen/src/App.vue
@@ -1,8 +1,29 @@
 <template>
   <div id="app">
+    
     <h1>{{ title }}</h1>
     <h2>{{currentDate()}}</h2>
 
+    <ul 
+      v-if="entries"
+        class="entry-list">     
+      <li        
+        class="entry-item"
+        v-for="entry in entries"      
+
+        :key="entry.id"
+      >
+        <span class="entry-daytime" style="color:red"><b>{{entry[0]}}, {{entry[1].replaceAll("/", ".")}}</b></span><br />
+        <h3 class="entry-title" style="color:coral">{{entry[2]}}</h3>
+        <span class="entry-description" style="color:coral">{{entry[3]}}</span><br />
+        
+        </li>
+      </ul>
+
+      <h1 v-else>No Items available</h1>
+      
+   
+    <!-- TESTDATEN (Zeile 70-78) nicht mehr vorhanden:
     <ul>
       <li style="color:red"><b>{{daytime1}}</b></li>
       <li style="color:coral"><b>{{description1}}</b></li>
@@ -20,6 +41,7 @@
        <li style="color:coral"><b>{{description3}}</b></li>
       <li style="color:coral">{{task3}}</li>
     </ul>
+    -->
 
     <footer>
       <img 
@@ -44,13 +66,16 @@
 </template>
 
 <script>
-
+import axios from "axios";
 export default {
   name: 'App',
   data() {
     return {
       title: "Welcome to Opportunity",
-      description1: "Basisbeschäftigung Besuch",
+      sheet_id: "1a81aI0Y8ViZO0tI92h2YSMqVQJ8hmNNMyMylXgvwiU4",
+      api_token: "AIzaSyA-qeDXOhEeQDA0vQf7LgkF7DQtGnAtmAU",
+      entries: [],
+/*    description1: "Basisbeschäftigung Besuch",                            TESTDATEN
       description2: "Gruppenarbeit",
       description3: "Abschluss",
       daytime1: "08.30" + " Uhr",
@@ -58,16 +83,32 @@ export default {
       daytime3: "16.30" + " Uhr",
       task1: "Interessierte für den zweiten Kurs werden uns besuchen",
       task2: "Workshop",
-      task3: "Wrap-Up",
+      task3: "Wrap-Up",                                    */
     }
   },
+  computed: {
+    gsheet_url() {
+      return `https://sheets.googleapis.com/v4/spreadsheets/${this.sheet_id}/values:batchGet?ranges=A2%3AE100&valueRenderOption=FORMATTED_VALUE&key=${this.api_token}`;
+    },
+  },
+  
   methods: {
     currentDate() {
       const current = new Date();
       const date = `${current.getDate()}.${current.getMonth()+1}.${current.getFullYear()}`;
       return date;
-    }
-  }
+      },
+
+    getData() {
+      axios.get(this.gsheet_url).then((response) => {
+      this.entries = response.data.valueRanges[0].values;
+        });
+    },
+  },
+    mounted() {
+      this.getData();
+    },
+  
 };
   
 </script>
@@ -97,21 +138,20 @@ body {
   display:flex; 
   flex-direction:column; 
 }
-ul {
+h3, span, li {
    background-color: #0F05A0;
    display: block;
    list-style-type: none; 
    list-style-type: none;    
    font-size: 28px;
-   margin: 60px;
-   padding-top: 37px;
-   padding-bottom: 37px;
+   margin: 10px;
+   padding-top: 10px;
    padding-left: 40px;
 }
 
 footer {
   background: white;
-  position: absolute;
+  position: fixed;
   margin-left: 0px;
   margin-right: 0px;
   bottom: 0px;
